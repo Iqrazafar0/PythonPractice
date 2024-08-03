8f2ad68bc208c3c2c3785c1f2c21e990bf69a86b
Iqra Zafar <iqraraozafar0@gmail.com>
2024-08-03 20:40:00 +0500

    Update Firstprogram.py

--- Firstprogram.py
+++ Firstprogram.py
@@ -1,25 +1,25 @@
 #Q1:WAP that input 2 nums and print there sum.
 #SOLVE:
-#first=int(input("enter first num :"))
-#second=int(input("enter second num : "))
+first=int(input("enter first num :"))
+second=int(input("enter second num : "))
 
-#print("sum =",first + second)
+print("sum =",first + second)
  
 #Q2:WAP to input side of aquare and print its area.
 #SOLVE:
-#side = float(input("enter square side :"))
-#print("area =",side * side)
+side = float(input("enter square side :"))
+print("area =",side * side)
 
 #Q3:WAP to input 2 floating point nums and print their average.
 #SOLVE:
-#a = float(input("first num :"))
-#b = float(input("second num :"))
-#print("average =",(a+b)/2)
+a = float(input("first num :"))
+b = float(input("second num :"))
+print("average =",(a+b)/2)
 
 #Q4:WAP to input 2 int nums,a and b.
 #print True if a is greater thanor equal to b if not print False
 #SOLVE:
-#a = int(input("first num :"))
-#b = int(input("second num :"))
-#print(a>=b)
+a = int(input("first num :"))
+b = int(input("second num :"))
+print(a>=b)
 
