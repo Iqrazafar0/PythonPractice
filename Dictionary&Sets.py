e65bf56a63210d817ba9ddf37fa571b2fb33eb15
Iqra Zafar <iqraraozafar0@gmail.com>
2024-08-09 10:47:40 +0500

    Add files via upload

---
+++ Dictionary&Sets.py
@@ -0,0 +1,39 @@
+                       #dic & set in python
+#Q1:Store following word meaning in python:
+#  table : "A piece of funiture." ,  cat : "A small animal.
+
+#dictionary = {
+#    "cat " :"a small animal",
+#    "table" :"a piece of furniture"
+#}
+#print(dictionary)
+
+#Q2:You are given a list of subjects for the students.assume 1 classrome is required for 1 subject.how  many CR is needed by all students.
+
+#subjects  = {"python","java","c++","python","javascript","java","python","java","c++","c"}
+#print(subjects)
+#print(len(subjects))
+
+#Q3:WAP to enter marks of 3 subjects from the user and store them in a dictionary.start with an empty dictionary & add one by one.use
+#subject name as key & mark as value.
+
+#marks = {}
+
+#x = int(input("enter phy:"))
+#marks.update({"phy" : x})
+
+#x = int(input("enter eng:"))
+#marks.update({"eng" : x})
+
+#x = int(input("enter bio:"))
+#marks.update({"chem" : x})
+
+#print(marks)
+
+#Q4:Figure out a way to store 9 & 9.0 as separate values in a set.(you can take help og built-in data types)
+
+#values = {
+#    ("float",9.0),
+#    ("int" ,9)
+#}
+#print(values)
