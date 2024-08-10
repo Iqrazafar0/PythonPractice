ac43f58881a0f6f04eccdb78e5b55594def58ab1
Iqra Zafar <iqraraozafar0@gmail.com>
2024-08-10 20:20:47 +0500

    Add files via upload

---
+++ List&Tuples.py
@@ -0,0 +1,35 @@
+                            #List & tuple in python
+
+#Q1:WAP to ask the user to enter names of their three favourite movies & store them in a list.
+
+#movies = []
+#mov1 = str(input("Enter your 1st movie name."))
+#mov2 = str(input("Enter your 2nd movie name."))
+#mov3 = str(input("Enter your 3rd movie name."))
+#list_append = [mov1 , mov2, mov3]
+#print(list_append)
+
+#Q2:WAP to check if a list contains a plindrom elements.(Hint:use copy()method)
+
+#list1 = ['a', 'b', 'c']
+#list2 = [1,2,3]
+
+#copy_list1 = list1.copy()
+#copy_list1.reverse()
+
+#if(copy_list1 == list1):
+#    print("A palindrom")
+#else:
+#    print("Not a palindrom")
+
+#Q3:WAP to count the number of students with the grade 'A' in following tupple.
+
+#grade = ('C','D','A','A','B','A')
+#print(grade.count('A'))
+
+#Q4:WAP to store the above values in a list and sort them from 'A'TO 'D'.
+
+#list = ['C','D','A','A','B','A']
+#list.sort()
+#print(list)
+
